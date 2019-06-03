50c13c431d17f46251fa3acf57d4769a6d804bb1
Angela <angela.vc0917@gmail.com>
2019-06-03 20:56:21 +0900

    Revert "Program changed."
    This reverts commit 1713493b9fa759e66879dc6b750fd7152ba0675b.

--- Game1/Assets/Scripts/Enemy.cs
+++ Game1/Assets/Scripts/Enemy.cs
@@ -5,21 +5,21 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     private float timeElapsed1 = 0.5f;
+    float time = 0.2f;
     float AttackedCnt = 3;
     public static float spd;
     public static Vector3 pos;
     public GameObject SExp;
-    public GameObject Obj;
-    public GameObject Enm;
+    GameObject Obj;
     
+
+
     // Start is called before the first frame update
     void Start()
     {
         float dir = Random.Range(91, 269);    //0~359°方向に移動.
         spd = Random.Range(1, 2.5f); //速さ1~2.5.
         SetVelocity(dir, spd);
-
-        Enm = gameObject;
     }
 
     // Update is called once per frame
@@ -43,6 +43,14 @@ public class Enemy : MonoBehaviour
             timeElapsed1 = 1f;
         }
 
+        if(AttackedCnt <= 0)
+        {
+            time -= Time.deltaTime;
+            if(time <= 0){
+                Destroy(gameObject);
+            }
+        }
+
         if (X < min.x - 0.5)
         {
             Destroy(gameObject);
@@ -59,17 +67,16 @@ public class Enemy : MonoBehaviour
             // 爆発エフェクトを生成する	
             GameObject LExplosionPrefab1 = (GameObject)Resources.Load("LExplosion");
             Instantiate(LExplosionPrefab1, transform.position, Quaternion.identity);
-            Destroy(Obj);
-            Destroy(gameObject);
 
         }
         else
         {
             // 爆発エフェクトを生成する	
             SExp = (GameObject)Resources.Load("SExplosion");
-            Obj = (GameObject)Instantiate(SExp, Enm.transform.position, Quaternion.identity);
+            //SmallExp.Attacked(c.gameObject);
+            Obj = (GameObject)Instantiate(SExp, transform.position, Quaternion.identity);
             // 作成したオブジェクトを子として登録
-            Obj.transform.parent = Enm.transform;
+            Obj.transform.parent = transform;
         }
         
     }
--- Game1/Assets/Scripts/SmallExp.cs
+++ Game1/Assets/Scripts/SmallExp.cs
@@ -5,7 +5,8 @@ public class SmallExp : MonoBehaviour
 {
 
     //GameObject Enemy;
-    float time = 0.4f;
+    public static GameObject homingObj;
+    float time = 0.2f;
 
     // Use this for initialization
     void Start()
@@ -16,13 +17,21 @@ public class SmallExp : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // transformを取得
+       // Transform myTransform = this.transform;
+
+        // objを親として設定
+       // myTransform.parent = homingObj.transform;
 
         time -= Time.deltaTime;
         if (time <= 0)
         {
-            Debug.Log(gameObject.transform.parent.gameObject.name);
-
             Destroy(this.gameObject);
         }
     }
+
+   // public static void Attacked(GameObject c)
+   // {
+    //    homingObj = c;
+   // }
 }
